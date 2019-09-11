5f6fb1af2605f73469400512e39941420023e36e
MRVFivem <49911917+MRVFivem@users.noreply.github.com>
2019-09-11 16:36:29 +0200

    Add files via upload

---
+++ esx_eden_mrv_garage/README.md
@@ -0,0 +1,16 @@
+﻿![inpound](https://imgur.com/Dv7wdbd)
+![garage](https://imgur.com/CHH7IoE)
+
+# As I no longer use esx, I decided to publish my modification of esx_eden_garage in NativeUILua_Reloaded.
+
+- Original script : [https://github.com/snyx95/esx_eden_garage]
+- NativeUILua_Reloaded : [https://github.com/iTexZoz/NativeUILua_Reloaded]
+- Add me on discord : mrv#0002 for support
+
+# In config.lua, you can:
+
+- Choose to use the mouse or not
+- Change the price of the impound
+- Change the language
+
+# Enjoy !
---
+++ esx_eden_mrv_garage/config.lua
@@ -0,0 +1,183 @@
+
+Config = {
+
+	DrawDistance = 100,
+	
+	Locale = 'en',
+
+	Price = 3000,
+
+	Mouse = true,
+	
+	BlipInfos = {
+		Sprite = 290,
+		Color = 38 
+	},
+	
+	BlipPound = {
+		Sprite = 67,
+		Color = 64 
+	}
+}
+
+Config.Garages = {
+
+	Garage_Centre = {	
+		Pos = {x=215.800, y=-810.057, z=30.727},
+		Size  = {x = 1.5, y = 1.5, z = 1.5},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 36,
+		SpawnPoint = {
+			Pos = {x=229.700, y= -800.1149, z= 30.5722},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Heading=157.84,
+			Marker = 36		
+		},
+		DeletePoint = {
+			Pos = {x=215.124, y=-791.377, z=30.646},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36,
+			
+		},
+		MunicipalPoundPoint = {
+			Pos = {x=482.896, y=-1316.557, z=29.301},
+			Color = {r=25,g=25,b=112},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		SpawnMunicipalPoundPoint = {
+			Pos = {x=490.942, y=-1313.067, z=28.964},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36,
+			Heading=299.42
+		},
+	},
+	
+	Garage_Paleto = {	
+		Pos = {x=105.359, y=6613.586, z=32.3973},
+		Size  = {x = 1.5, y = 1.5, z = 1.5},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 36,
+		SpawnPoint = {
+			Pos = {x=128.7822, y= 6622.9965, z= 31.7828},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		DeletePoint = {
+			Pos = {x=126.3572, y=6608.4150, z=31.8565},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		MunicipalPoundPoint = {
+			Pos = {x=-185.187, y=6272.027, z=31.580},
+			Color = {r=25,g=25,b=112},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},	
+		SpawnMunicipalPoundPoint = {
+			Pos = {x=-199.160, y=6274.180, z=31.580},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+	},
+	
+	Garage_Poissoniers = {	
+		Pos = {x = -1511.6556396484,y = 5009.875,z = 62.800533294678 },
+		Size  = {x = 1.5, y = 1.5, z = 1.5},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 36,
+		SpawnPoint = {
+			Pos = {x = -1511.6556396484,y = 5009.875,z = 62.800533294678 },
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		DeletePoint = {
+			Pos = {x = -1524.1857910156,y = 4997.2885742188,z = 62.598690032959 },
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		MunicipalPoundPoint = {
+			Pos = {x = -2211.8347167969,y = 4241.1069335938,z = 47.61901473999 },
+			Color = {r=25,g=25,b=112},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},	
+		SpawnMunicipalPoundPoint = {
+			Pos = {x = -2205.4594726563,y = 4247.3603515625,z = 47.639003753662 },
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+	},
+	
+	Garage_SandyShore = {	
+		Pos = {x = 1501.2,y = 3762.19,z = 34.0 },
+		Size  = {x = 1.5, y = 1.5, z = 1.5},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 36,
+		SpawnPoint = {
+			Pos = {x = 1497.15,y = 3761.37,z = 33.8 },
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		DeletePoint = {
+			Pos = {x = 1504.1,y = 3765.55,z = 33.8 },
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		MunicipalPoundPoint = {
+			Pos = {x = 1561.6000976563,y = 3522.8583984375,z = 35.789356231689 },
+			Color = {r=25,g=25,b=112},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},	
+		SpawnMunicipalPoundPoint = {
+			Pos = {x = 1551.8347167969,y = 3518.1003417969,z = 35.988235473633 },
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+	},
+	
+		Garage_Aeroport = {	
+		Pos = {x = -977.21661376953,y = -2710.3798828125,z = 13.853487014771 },
+		Size  = {x = 1.5, y = 1.5, z = 1.5},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 36,
+		SpawnPoint = {
+			Pos = {x = -977.21661376953,y = -2710.3798828125,z = 13.853487014771 },
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		DeletePoint = {
+			Pos = {x = -966.88208007813,y = -2709.9028320313,z = 13.83367729187 },
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+		MunicipalPoundPoint = {
+			Pos = {x = -1041.4571533203,y = -2676.3471679688,z = 13.830760002136 },
+			Color = {r=25,g=25,b=112},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},	
+		SpawnMunicipalPoundPoint = {
+			Pos = {x = -1048.3468017578,y = -2669.771484375,z = 13.830758094788 },
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 1.5, y = 1.5, z = 1.5},
+			Marker = 36
+		},
+	},
+
+}
