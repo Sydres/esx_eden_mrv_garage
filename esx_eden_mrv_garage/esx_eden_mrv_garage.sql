502ec1ccb7b71de177ef4848b744beb62d1b052b
MRVFivem <49911917+MRVFivem@users.noreply.github.com>
2019-09-11 17:15:09 +0200

    Update esx_eden_mrv_garage.sql

--- esx_eden_mrv_garage/esx_eden_mrv_garage.sql
+++ esx_eden_mrv_garage/esx_eden_mrv_garage.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `owned_vehicles` (
 	`owner` varchar(22) NOT NULL,
 	`plate` varchar(12) NOT NULL,
-    `inpound` tinyint(1) DEFAULT 0,
+    `impound` tinyint(1) DEFAULT 0,
 	`vehicle` longtext,
 	`type` VARCHAR(20) NOT NULL DEFAULT 'car',
 	`job` VARCHAR(20) NULL DEFAULT NULL,
